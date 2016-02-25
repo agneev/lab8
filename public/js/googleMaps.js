ef19729244dcdcdb82e671ddd887eeb8c502f81c
vagrant <vagrant@precise32.(none)>
2016-02-25 01:32:45 +0000

    asd

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,7 +1,7 @@
 function initMap() {
   // Create center marker at UCSD
   var ucsd_ltlng = {lat:32.880, lng:-117.236};
-
+  var house = {lat: 32.8738446, lng:-117.2265816};
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
     center: ucsd_ltlng,
@@ -12,5 +12,11 @@ function initMap() {
       position: ucsd_ltlng,
       map: map,
       title: 'UCSD'
+  });
+
+  var h_marker = new google.maps.Marker({
+      position: house,
+      map: map,
+      title: 'Home'
   });
 }
