3b40474f972b55418f4e6e86e74f9621e47045be
vagrant <vagrant@precise32.(none)>
2016-02-25 01:21:17 +0000

    asd

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,13 +1,14 @@
 function initCamera() {
   //Uncomment and fill in the correct selectors below.
-  //capture($('<Your code here>'),
-  //        $('<Your code here>'),
-  //        $('<Your code here>'));
+  capture($('#camera-video'),
+          $('#camera-canvas'),
+          $('#camera-button'));
 }
 
 function capture(video, canvas, snapshotButton) {
   //Adopted from https://dev.opera.com/articles/media-capture-in-mobile-browsers/
   //Setup navigator for all versions of browsers.
+  console.log(canvas);
   navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
               navigator.mozGetUserMedia || navigator.msGetUserMedia;
   var ctx = canvas[0].getContext('2d');
